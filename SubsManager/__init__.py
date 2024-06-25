9be1c2537e9fbc3a9f1553c14725339745b74fb8
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2024-06-25 11:55:57 +0530

    Update __init__.py

--- SubsManager/__init__.py
+++ SubsManager/__init__.py
@@ -79,7 +79,7 @@ bot_chats = {
      -1002025167901: { 
         "prices": {"365d": 99,  "730day": 199},        "sshots": [ 
             "https://telegra.ph/file/79fb5774c5cb3a8c44853.jpg",           
-            "https://telegra.ph/file/e1e1198bdba65e775d2ee.jpg",       ], 
+        "https://telegra.ph/file/e1e1198bdba65e775d2ee.jpg",       ], 
         "args": {     
             "Type": "bra panty ", 
             "Desp": "Nothing to Say, Highly Recommended bra panty weared videos photos",}, 
