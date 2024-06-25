6999adf7dd6655f4af55b9b632cc946cd23ba3a9
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2024-06-25 11:33:51 +0530

    Update __init__.py

--- SubsManager/__init__.py
+++ SubsManager/__init__.py
@@ -61,14 +61,6 @@ bot_chats = {
             "Type": "R@pe videos", 
             "Desp": "Nothing to Say, Highly Recommended rape videos",}, 
      },
-     -1002035263833: 
-     {"prices": {"365d": 99,  "730day": 199},        "sshots": [ 
-            "https://telegra.ph/file/bec5e9ba05e9a8fb384e8.jpg",           
-            "https://telegra.ph/file/d92635036084702b8c1cf.jpg", ], 
-        "args": {       
-            "Type": "shemale videos", 
-            "Desp": "Nothing to Say, Highly Recommended shemale videos",}, 
-     },
      -1002012075373: { 
         "prices": {"365d": 99,  "730day": 199},        "sshots": [ 
             "https://telegra.ph/file/b061bd85ee8a41565616e.jpg",                 ], 
