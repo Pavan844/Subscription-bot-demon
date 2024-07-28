f5331bb7d9d2f7f84cf4c627677498789c9bd722
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2024-07-28 23:15:19 +0530

    Update start.py

--- SubsManager/plugins/start.py
+++ SubsManager/plugins/start.py
@@ -39,7 +39,7 @@ async def start_msg(_, message):
                     InlineKeyboardButton("My Plans", callback_data="cbbot myplan"),
                     InlineKeyboardButton("My Cart", callback_data="cbbot mycart"),
                     InlineKeyboardButton("Refer and earn", callback_data="cbbot ref"),
-                    InlineKeyboardButton("HOW TO BUY ", callback_data="https://t.me/vipinsidersbackup/6"),
+                    InlineKeyboardButton('✪ how to buy', url="https://t.me/vipinsidersbackup/6"),
                 ],
                 [InlineKeyboardButton("Close", callback_data="cbbot close")],
             ]
